2e852788b8cf1fd96be945c821f64668a6dd05cb
SR <saodat.riskiyeva@gmail.com>
2025-08-20 00:00:41 -0400

    searching the DOM for elements

--- app.js
+++ app.js
@@ -2,4 +2,8 @@
 
 // console.dir(document.body.children[0]);
 
-document.body.children[1].children[0].href = "https://www.google.com";
+// document.body.children[1].children[0].href = "https://www.google.com";
+
+let anchorElement = document.getElementById("external-link");
+
+anchorElement.href = "https://www.google.com";
