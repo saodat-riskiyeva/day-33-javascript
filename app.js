33de31c94c8f90b2b323346a78f5b43dd1c4e6c0
SR <saodat.riskiyeva@gmail.com>
2025-08-19 23:56:17 -0400

    drilling into the DOM

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-console.dir(window.document);
+// console.dir(window.document);
+
+// console.dir(document.body.children[0]);
 
 document.body.children[1].children[0].href = "https://www.google.com";
