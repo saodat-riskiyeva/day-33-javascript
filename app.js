93fd11ef99cbe4dfd3dafd42aa0c3d67f7036527
SR <saodat.riskiyeva@gmail.com>
2025-08-20 19:21:42 -0400

    adding eventListener

---
+++ app.js
@@ -0,0 +1,6 @@
+let paragraphElement = document.querySelector("p");
+
+function changeParagraphText() {
+  paragraphElement.textContent = "Clicked!";
+}
+paragraphElement.addEventListener("click", changeParagraphText);
