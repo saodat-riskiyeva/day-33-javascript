7f4dbd4b9670c2e19dd639d2dcdd80ec6d6abdc8
SR <saodat.riskiyeva@gmail.com>
2025-08-20 20:37:01 -0400

    proposed solution

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-let inputElement = document.querySelector("input");
-let remainingText;
+// let inputElement = document.querySelector("input");
+// let remainingText;
 
-function logInputValue(event) {
-  remainingText = 60 - event.target.value.length;
+// function logInputValue(event) {
+//   remainingText = 60 - event.target.value.length;
+// }
+
+// inputElement.addEventListener("input", logInputValue);
+
+let productNameInputElement = document.getElementById("product-name");
+let remainingTextElement = document.getElementById("remaining-chars");
+
+let maxAllowedChars = productNameInputElement.maxLength;
+
+function updateRemainingCharacters(event) {
+  const remainingTextChars = maxAllowedChars - event.target.value.length;
+
+  remainingTextElement.textContent = remainingTextChars;
 }
 
-inputElement.addEventListener("input", logInputValue);
+productNameInputElement.addEventListener("input", updateRemainingCharacters);
